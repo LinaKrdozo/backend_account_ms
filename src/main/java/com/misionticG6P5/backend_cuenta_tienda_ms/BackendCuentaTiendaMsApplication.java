7efe42b8fe8d9601754d2ab99577c481e244c6fc
Linakrdozo <linakrdozo13@gmail.com>
2021-11-23 20:16:00 -0500

    initial settings to project

---
+++ src/main/java/com/misionticG6P5/backend_cuenta_tienda_ms/BackendCuentaTiendaMsApplication.java
@@ -0,0 +1,13 @@
+package com.misionticG6P5.backend_cuenta_tienda_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCuentaTiendaMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCuentaTiendaMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misionticG6P5/backend_cuenta_tienda_ms/BackendCuentaTiendaMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misionticG6P5.backend_cuenta_tienda_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCuentaTiendaMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
